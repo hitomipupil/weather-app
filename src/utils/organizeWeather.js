de153d7666daa3a2fcea8cb4bffbaa85dd0c94c1
Hitomi <hitomi0727pupil@gmail.com>
2024-08-13 16:41:13 +0200

    Initial commit

---
+++ src/utils/organizeWeather.js
@@ -0,0 +1,54 @@
+const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+const organizeWeather = (weather) => {
+    const { time, temperature_2m } = weather.hourly;
+    const days = [];
+
+    let oldTemp;
+    time.forEach((hour, index) => {
+        const now = new Date(hour);
+
+        const day = now.getDate();
+        const dayOfWeek = weekDays[now.getDay()];
+        let hours = now.getHours();
+        let minutes = now.getMinutes();
+        let seconds = now.getSeconds();
+
+        hours = hours < 10 ? `0${hours}` : hours;
+        minutes = minutes < 10 ? `0${minutes}` : minutes;
+        seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+        const temp = temperature_2m[index];
+        const formattedTemp = temp.toFixed(1);
+
+        let arrow;
+        if (oldTemp) {
+            if (oldTemp > temp) {
+                arrow = "down";
+            } else if (oldTemp < temp) {
+                arrow = "up";
+            } else {
+                arrow = "same";
+            }
+        } else {
+            arrow = "none";
+        }
+
+        const hourAndTemp = {
+            hour: `${hours}:${minutes}:${seconds}`,
+            temp: formattedTemp,
+            arrow: arrow
+        };
+
+        if (!days[day]) {
+            days[day] = { dayOfWeek, data: [hourAndTemp] };
+        } else {
+            days[day].data.push(hourAndTemp);
+        }
+        oldTemp = temp;
+    });
+
+    return days;
+};
+
+export default organizeWeather;
